ca4653383486d3bb6061b2fc22cef8fa63fe2354
Grace Li <Grace@GraceLi-MacbookPro.local>
2015-08-10 19:02:59 -0700

    Initial Commit

---
+++ tips/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tips
+//
+//  Created by Grace Li on 8/10/15.
+//  Copyright (c) 2015 grace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
